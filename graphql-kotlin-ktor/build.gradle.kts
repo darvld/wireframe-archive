9a1116c34cbc211917522e0d2f0032738418ee89
Dario Valdespino <dvaldespino00@gmail.com>
2022-07-16 13:28:08 -0400

    Configure publishing for runtime library.

--- graphql-kotlin-ktor/build.gradle.kts
+++ graphql-kotlin-ktor/build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
+    id("maven-publish")
 }
 
 dependencies {
-    implementation(libs.graphql.java)
+    api(libs.graphql.java)
 
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.serialization)
@@ -15,6 +16,16 @@ dependencies {
 
 kotlin {
     explicitApi()
+}
+
+java {
+    withSourcesJar()
+}
+
+publishing {
+    publications.create<MavenPublication>("runtime") {
+        from(components["java"])
+    }
 }
 
 tasks.test {
